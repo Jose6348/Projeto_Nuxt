314b80cd765d2885b64d522cc4f4e34cd22c70fa
Jose Jorge <jfalasco88@gmail.com>
2024-10-13 20:40:48 -0300

    First Page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  alias: {
+    css: '@/assets/css',
+  },
+  app: {
+    baseURL: process.env.BASE_URL || '',
+  },
+  css: ['~/assets/css/main.css'],
+  dir: {
+    layouts: 'layouts',       
+    middleware: 'middleware', 
+    pages: 'pages',           
+    static: 'static',         
+  },
+});
